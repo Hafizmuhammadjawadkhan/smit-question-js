fe2da1e636cfa0499dcecda5ead67449281633d5
MUHAMMAD JAWAD KHAN <khanmjawad35@gmail.com>
2024-02-28 22:39:09 +0500

    java question

---
+++ assignment.js
@@ -0,0 +1,173 @@
+// var forMeet="WELLCOME"
+// var error="Error! Please enter a valid password"
+// var codLand="Wellcome to js Land.....Happy Coding!"
+// var land="Wellcome to js Land....."
+//  var coding="Happy Coding!"
+ 
+// alert("WELLCOME")
+// alert(error)
+// alert(codLand)
+// alert(land)
+// alert(coding)
+
+
+// 1. var x = 25 and var y = 7, what is the result of x % y?
+
+// 2. If var a = 15 and var b = 4, what is the value of a * b after adding 10 to a and subtracting 2 from b?
+
+// 3. Suppose var c = 12 and var d = 3, calculate the result of c ** d.
+
+// 4. For var e = 10 and var f = 3, what is the value of e += f * 2?
+
+// 5. Considering var g = 20 and var h = 4, what will be the outcome of g / h after adding 5 to g?
+
+// 6. Given var i = 8 and var j = 3, determine the value of i * j after subtracting 2 from i.
+
+// 7. If var k = 18 and var l = 5, what is the result of k - l + 2?
+
+// 8. Suppose var m = 7 and var n = 2, calculate the value of m ** n.
+
+// 9. For var o = 13 and var p = 4, what will be the outcome of o % (p + 2)?
+
+// 10. Given var q = 16 and var r = 3, determine the result of q * r after dividing q by 2.
+
+// 11. If var s = 25 and var t = 3, what is the value of s / t after subtracting 1 from s?
+
+// 12. Suppose var u = 11 and var v = 3, calculate the value of u - v after adding 2 to v.
+
+// 13. For var w = 22 and var x = 5, what will be the outcome of w + (x ** 2)?
+
+// 14. Given var y = 19 and var z = 4, determine the result of y % (z + 1).
+
+// 15. If var a = 10 and var b = 3, what is the value of a * (b - 2)?
+
+//Question no.1
+var x = 25
+var y = 7
+var modulas =25 % 7
+console.log(modulas)
+
+
+
+//Question no.2
+//a=15 ,b=4 after adding 10 to a and subtracting 2 from b
+var a = 25
+var b = 2
+var multiplication = 25 * 2 
+console.log(multiplication)
+
+
+
+//Question no.3
+var c =12 
+var d =3 
+var exponent = 12 ** 3
+console.log(exponent)
+
+
+
+//Question no.4
+//f=3 what is the value of e += f * 2
+var e = 10
+var f = 6
+var addition = 10 + 6
+console.log(addition)
+
+
+
+//Question no.5
+//g = 20 what will be the outcome of g / h after adding 5 to g
+var g = 25
+var h = 4
+var division = 25 / 4
+console.log(division) 
+
+
+
+//Question no.6
+//i=8 ,j=3 after adding 10 to a and subtracting 2 from i
+var i = 8
+var j = 3
+var multiplication = 8 * 3
+console.log(multiplication)
+
+
+
+//Question no.7
+//k=18 ,l=5
+var k = 18
+var l = 5
+var bodmas = k - l + 2
+console.log(bodmas)
+
+
+
+//Question no.8
+var m = 7 
+var n = 2
+var exponent = 7 ** 2
+console.log(exponent)
+
+
+
+//Question no.9
+//p=4 what will be the outcome of o % (p + 2)
+var o = 13 
+var p = 6
+var modulas =  13 % 6
+console.log(modulas)
+
+
+
+//Question no.10
+//q=16 determine the result of q * r after dividing q by 2
+var q = 8
+var r = 3
+var multiplication =  8 * 3
+console.log(multiplication)
+
+
+
+
+//Question no.11
+//s=25 what is the value of s / t after subtracting 1 from s
+var s = 24
+var t = 3
+var division =  24 / 3
+console.log(division)
+
+
+
+//Question no.12
+//v=3 calculate the value of u - v after adding 2 to v.
+var u = 11
+var v = 5
+var subtraction =  11 - 5
+console.log(subtraction)
+
+
+
+//Question no.13
+//x=5 what will be the outcome of w + (x ** 2)
+var w = 22
+var x = 25
+var addition =  22 + 25
+console.log(addition)
+
+
+
+//Question no.14
+//z=4 determine the result of y % (z + 1)..
+var y = 19
+var z = 5
+var modulas =  19 % 5
+console.log(modulas)
+
+
+
+//Question no.15
+//b=3 what is the value of a * (b - 2).
+var a = 10
+var b = 1
+var multiplication =  10 * 1
+console.log(multiplication)
